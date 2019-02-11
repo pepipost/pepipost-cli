fab2834ca56b4190c57d7aaada6d313cf0eb4662
vikram-sahu <hellovikramsahu@gmail.com>
2019-02-11 13:37:27 +0530

    renamed

--- internal/apimail/apimail.go
+++ internal/apimail/apimail.go
@@ -8,8 +8,8 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	b64 "encoding/base64"
-	"github.com/pepipost/pepipost_lib/pepipost_pkg"
-	"github.com/pepipost/pepipost_lib/models_pkg"
+	"github.com/pepipost/pepipost-sdk-go/pepipost_lib/pepipost_pkg"
+	"github.com/pepipost/pepipost-sdk-go/pepipost_lib/models_pkg"
 	"github.com/briandowns/spinner"
 	"github.com/urfave/cli"
 )
